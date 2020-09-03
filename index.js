38c96a70892b2bc004a66ef15a8beca082e2948a
kait-IssaAnagreh <IssaAnagreh@Isas-MacBook-Pro.local>
2020-09-03 22:50:58 +0300

    install antd

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ import App from './src/App';
 import {name as appName} from './app.json';
 
 console.disableYellowBox = true;
+// LogBox.ignoreAllLogs(value);
 
 AppRegistry.registerComponent(appName, () => App);
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
-import {
-  View,
-  Text,
-} from 'react-native';
+import React, {useState} from 'react';
+import {View} from 'react-native';
 import styles from './styles';
+import SearchBar from '@ant-design/react-native/lib/search-bar';
 
 interface Props {}
 
 export default (props: Props) => {
+  const [state, setState] = useState('');
+
+  const onChange = (value: any) => {
+    setState(value);
+  };
+
+  const clear = () => {
+    setState('');
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>
-          Here we are
-      </Text>
+      <SearchBar
+        value={state}
+        placeholder="Search"
+        onSubmit={value => console.log(value, 'onSubmit')}
+        onClear={value => console.log(value, 'onClear')}
+        onFocus={() => console.log('onFocus')}
+        onBlur={() => console.log('onBlur')}
+        onCancel={() => console.log('onCancel')}
+        showCancelButton
+        onChange={onChange}
+      />
     </View>
   );
 };
